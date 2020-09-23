eea9ffaae02034955c4b9e9f8de22e31bff03964
danghoanglong719 <danghoanglong1919@gmail.com>
2020-09-23 22:42:33 +0700

    lab-01

---
+++ tet/tet/Test Suites/LoginForm.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginForm</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b2d6bec7-7a49-4850-9244-675803ee17c9</testSuiteGuid>
+   <testCaseLink>
+      <guid>e31cb975-fc61-41de-ae7f-6795afacf620</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/001-Success Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b7a17953-e7ed-4d7a-9c9c-4fe10410d113</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/002-Password Fail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>54ca82ab-4df3-42df-9b28-618cdf481554</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4e105ad2-50c1-4c66-9c1f-3b31152c571a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/004</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
